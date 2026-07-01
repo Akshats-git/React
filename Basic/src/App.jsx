f7c4f02a2bcd782e478dba36f69f66b038c39afc
Akshat <akshatgupta099@gmail.com>
2026-07-01 20:19:22 +0530

    implement custom rendering logic and create initial HTML structure

--- Basic/src/App.jsx
+++ Basic/src/App.jsx
@@ -1,9 +1,10 @@
 
 function App() {
-
+  const username = "Akshat"
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello, {username}!</h1>
+      <h1>this is {2+2}</h1>
     </>
   )
 }
--- Basic/src/main.jsx
+++ Basic/src/main.jsx
@@ -1,9 +1,44 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://www.google.com",
+        target: '_blank'
+    },
+    children: 'Click me to visit Google'
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  )
+}
+
+const AnotherElement = (
+  <a href="http://google.com" target="_blank">
+    Click me to visit Google
+  </a>
+)
+
+const areactElement = React.createElement(
+  'a',
+  { href: 'https://www.google.com', target: '_blank' },
+  'Click me to visit Google'
+  // the expressions inside the curly braces will be evaluated and the result will be rendered as the content of the anchor tag
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>,
+  // <MyApp />
+  // AnotherElement
+  // reactElement wont work here because it is not a valid React element, it is just a plain JavaScript object.
+  // areactElement //lowercase works here because it is a valid React element created using React.createElement
 )
